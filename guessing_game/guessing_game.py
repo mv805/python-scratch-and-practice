684c7c649242e120b4fc0c43f251e7e3dbd10bcc
Matt Villa <matthewvilla805@gmail.com>
2023-03-05 11:09:23 -0800

    added game logic

--- guessing_game/guessing_game.py
+++ guessing_game/guessing_game.py
@@ -2,28 +2,82 @@ import random
 
 
 class GuessingGame:
-
+    """A guessing game"""
     # attributes
-    guesses = 0
-    MAX_SECRET_NUMBER = 100
-    MIN_SECRET_NUMBER = 1
+    _guesses = 0
+    _win = False
+    _game_result = ''
+    _last_guess = None
 
-    def __init__(self):
-        secret_number = random.randint(
-            self.MIN_SECRET_NUMBER, self.MAX_SECRET_NUMBER)
+    def __init__(self, minGuess=100, maxGuess=1):
+
+        if maxGuess > 100:
+            maxGuess = 100
+        if minGuess < 100:
+            minGuess = 1
+
+        self._secret_number = random.randint(
+            minGuess, maxGuess)
 
     def getGuesses(self):
-        return self.guesses
+        return self._guesses
 
-    def takeGuess(self):
-        guess = input("What is your guess?")
-        if (guess > 100 or guess < 1):
-            print("OUT OF BOUNDS")
+    def getSecretNumber(self):
+        return self._secret_number
+
+    def getWinStatus(self):
+        return self._win
+
+    def submitGuess(self, guess):
+        if (guess == self._secret_number):
+            self._game_result = "You Win!"
+            self._win = True
         else:
-            self._compareGuess(guess)
+            if (self._guesses == 0):
+                self._game_result = "Nope, try another guess"
+                self._last_guess = guess
+            elif (guess > self._last_guess and self._last_guess > self._secret_number):
+                self._game_result = "Colder!"
+            elif (guess < self._last_guess and guess < self._secret_number):
+                self._game_result = "Colder!"
+            elif (guess > self._last_guess and guess < self._secret_number):
+                self._game_result = "Warmer!"
+            elif (guess < self._last_guess and guess > self._secret_number):
+                self._game_result = "Warmer!"
 
-    def _compareGuess(self, guess):
-        pass
-    
-    def _checkWin(self):
-        pass
+            self._guesses += 1
+        
+
+    def getResult(self):
+        return self._game_result
+
+
+def game():
+
+    game = GuessingGame(1, 10)
+    print("Game is starting...")
+
+    while (not game.getWinStatus()):
+
+        guess = int(input("Submit a guess: "))
+
+        if (guess < 1 or guess > 100):
+            print("Out of bounds guess, try again.")
+            continue
+
+        game.submitGuess(guess)
+        print(game.getResult())
+
+    print(f"Good job you won! It took you {game.getGuesses()} guesses to win.")
+
+
+game()
+# establish game
+
+# get the guess
+# check win
+# return status
+
+# after win
+
+# confirm win and how many guesses it took
